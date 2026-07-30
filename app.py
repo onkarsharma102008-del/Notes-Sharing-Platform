2b28f23a108e913b48d918df7cfcc2e87356cd6d
Onkar Sharma <onkarsharma10062008@gmail.com>
2026-07-30 21:15:23 +0530

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -600,4 +600,6 @@ def course_notes(course_name):
     return render_template("all_notes.html", notes=notes)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
+    # app.run(debug=True)
