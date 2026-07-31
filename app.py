13ad51f32cc6fa9c114aa35963504b1e3fd7d1ed
Onkar Sharma <onkarsharma10062008@gmail.com>
2026-07-31 07:53:43 +0530

    Added database initialization

--- app.py
+++ app.py
@@ -599,6 +599,25 @@ def course_notes(course_name):
 
     return render_template("all_notes.html", notes=notes)
 
+    def init_db():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fullname TEXT,
+        email TEXT,
+        username TEXT,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
